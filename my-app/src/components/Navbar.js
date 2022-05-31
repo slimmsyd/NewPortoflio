2d2289607f29e3789d4bc991a43634ab86e502e0
sanders <ssanderss444@gmail.com>
2022-05-31 19:09:07 -0400

    moble burger menu

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,6 +1,9 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import '../styles/Navbar.scss'
 const Navbar = () => { 
+
+    const [navigation, setNavigation] = useState(false);
+
   
     const scrollSkills = () => { 
         window.scrollTo({
@@ -22,24 +25,62 @@ const Navbar = () => {
         })
     }
 
+    //functions to showNav
+    function showNav() {
+        setNavigation(true);
+        if(navigation) { 
+            console.log("Its true")
+            setNavigation(false)
+            
+        }else { 
+            console.log("its false now")
+        }
+        
+    }
+
+
 return ( 
     <>
     
 
     <nav className = "nav">
-    <div>
-     <h1 onClick={scrollSkills} > Skills</h1>
-    </div>
-    <div>
-    <h1 onClick={scrollHome}> Home</h1>
+        <div>
+        <h1 onClick={scrollSkills} > Skills</h1>
+        </div>
+        <div>
+        <h1 onClick={scrollHome}> Home</h1>
+
+        </div>
+
+        <div>
+        <h1 onClick={scrollContact} > Contact</h1>
+
+        </div>
+    
+    </nav>
+
+
+
+    <nav onClick={showNav} className = "menuBurger">
+        <img src = "/BurgerMenu.png" className ="img" alt ="burgermenu"></img>
+    </nav>
+
+    <div className ={`hamburger_Div ${navigation ? "hamburgerDivShown": "" }`}>
+        
+        <ul className = "navLinks">
+           
+            <li><a href = "https://twitter.com/slimmsyd">Twitter</a></li>
+        </ul>
+        <ul className = "navLinks">
+            <li><a href = "https://www.linkedin.com/in/sydney-sanders-dev/">LinkedIn</a></li>
+        </ul>
+        <ul className = "navLinks">
+            <li><a href = "https://www.youtube.com/channel/UCC8yLhVL7lThf295DtD4rag">Youtube</a></li>
+        </ul>
 
     </div>
 
-    <div>
-    <h1 onClick={scrollContact} > Contact</h1>
-
-    </div>
-</nav>
+    
 
 
 </>
